f73ba75a46c02851a76e60b15f4e93085cf71bc2
AgustinCastanol <acastanol@fi.uba.ar>
2022-05-08 01:01:31 -0300

    change extension from contact at jsx for incompatibility

--- src/components/ContactComponent.jsx
+++ src/components/ContactComponent.jsx
@@ -2,15 +2,13 @@ import {MdOutlineEmail} from 'react-icons/md'
 import { BsWhatsapp } from 'react-icons/bs'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
-import { LegacyRef} from 'react';
 
 export default function ContactComponent() {
-  const form =useRef() as LegacyRef<HTMLFormElement> | undefined;
-  let ref:HTMLFormElement
-  const sendEmail= (e:any)=>{
-  if(form !== null && form !== undefined && ref !== undefined){
+  const form =useRef();
+  const sendEmail= (e)=>{
+  if(form !== null && form !== undefined){
     e.preventDefault();
-    emailjs.sendForm('service_k9r3xl9','template_bmhbvxl',ref.current,'40P4k4I586r-sk3M1')
+    emailjs.sendForm('service_k9r3xl9','template_bmhbvxl',form.current,'40P4k4I586r-sk3M1')
   }
   e.target.reset()
   }
