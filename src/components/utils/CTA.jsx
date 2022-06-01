b3a03146f404d390f5937687cbe403a18637d186
AgustinCastanol <acastanol@fi.uba.ar>
2022-06-01 08:18:39 -0300

    Fixed button cta

--- src/components/utils/CTA.jsx
+++ src/components/utils/CTA.jsx
@@ -2,7 +2,7 @@ import CV from "../../assets/pdf/CV-AgustinCastañol.pdf"
 export default function CTA(){
     return (
     <div className="cta">
-        <a href={CV} className="btn">Download CV</a>
-        <a href='#contact' download className='btn btn-primary '>Let's Talk</a>
+        <a href={CV} download className="btn">Download CV</a>
+        <a href='#contact' className='btn btn-primary '>Let's Talk</a>
     </div>)
 }
